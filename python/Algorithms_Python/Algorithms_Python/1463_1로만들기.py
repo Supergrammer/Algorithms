579f5d2b3028eff3e2ea04eb179768543c8bda19
Supergrammer <supergrammer@naver.com>
2020-07-18 12:32:59 +0900

    Algorithms(Python) - 2020_07_18

---
+++ python/Algorithms_Python/Algorithms_Python/1463_1로만들기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def DP(n):
+	if n <= 1:
+		return 0
+	if memoi[n] != -1:
+		return memoi[n]
+	
+	div3 = DP(n // 3) + n % 3 + 1
+	div2 = DP(n // 2) + n % 2 + 1
+
+	memoi[n] = min(div3, div2)
+	return memoi[n]
+
+N = int(input())
+memoi = [-1 for _ in range(1000001)]
+print(DP(N))
